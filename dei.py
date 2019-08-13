4c9b3e7047f2828b1da499d849d13b8f3a390a1a
Rajadeivani <53109351+Rajadeivani@users.noreply.github.com>
2019-08-13 14:59:17 +0530

    Create dei.py

---
+++ dei.py
@@ -0,0 +1,13 @@
+lower = 900
+upper = 1000
+
+
+print("lower,upper,")
+
+for n in range(lower,upper + 1):
+   if n > 1:
+       for i in range(2,n ):
+           if (n % i) == 0:
+               break
+       else:
+           print(n)
